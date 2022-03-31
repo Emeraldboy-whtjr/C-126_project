72f71de97b03454d91f34560a3fb870368a81808
Emeraldboy-whtjr <82824596+Emeraldboy-whtjr@users.noreply.github.com>
2022-03-31 15:00:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 var righthand = "";
 var lefthand = "";
 
+var rightWrist_X = 0;
+var rightWrist_Y = 0;
+var leftWrist_X = 0;
+var leftWrist_Y = 0;
+
+var scoreLeftWrist = 0;//for storing the score of the left wrist.
+
+var statusSong = "";//this variable is to see wether the song is bieng played or not.
+
 function preload(){
     righthand = loadSound("Crying_for_Rain.mp3");
     lefthand = loadSound("Mirror_heart.mp3");
@@ -12,8 +21,52 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    posenet = ml5.poseNet(video, modelLoaded);//initialising poseNet model inside posenet variable.
+    posenet.on("pose", gotposes);//executing poseNet model.
+}
+
+function modelLoaded(){
+    console.log("model has loaded");
+
+}
+
+function gotposes(results){
+
+    if(results.length > 0){
+        console.log(results);
+
+        //getting x and y co-ordinates for right wrist.
+        rightWrist_X = results[0].pose.rightWrist.x;
+        rightWrist_Y = results[0].pose.rightWrist.y;
+
+        //getting x and y co-ordinates for left wrist.
+        leftWrist_X = results[0].pose.leftWrist.x;
+        leftWrist_Y = results[0].pose.leftWrist.y;
+
+        //for storing the score of the left wrist.
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        //for checking the score of the left wrist.
+        console.log(scoreLeftWrist);
+    }
 }
 
 function draw(){
-    image(video,100,100,400,400);
+    image(video,0,0,400,400);
+
+    fill("red");
+    
+
+    statusSong = lefthand.isPlaying();//to store the info if the song is bieng played or not.
+
+    //idk wtf is going on.
+    if(scoreLeftWrist > 0.2){
+        circle(leftWrist_X,leftWrist_Y,25);
+        righthand.stop();
+
+        if(righthand.isPlaying() == false){
+            righthand.play();
+            document.getElementById("song_pyed").innerHTML = "Crying for Rain";
+        }
+    }
 }
