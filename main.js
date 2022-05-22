704a29d7e54fabd666aa62a85c2b4b58adedf586
Emeraldboy-whtjr <82824596+Emeraldboy-whtjr@users.noreply.github.com>
2022-05-22 21:32:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var leftWrist_X = 0;
 var leftWrist_Y = 0;
 
 var scoreLeftWrist = 0;//for storing the score of the left wrist.
+var scoreRightWrist = 0;//for storing the score of the right wrist.
 
 var statusSong = "";//this variable is to see wether the song is bieng played or not.
 
@@ -48,6 +49,13 @@ function gotposes(results){
         scoreLeftWrist = results[0].pose.keypoints[9].score;
         //for checking the score of the left wrist.
         console.log(scoreLeftWrist);
+
+        //for storing the score of the right wrist.
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        //for checking the score of the right wrist.
+        console.log(scoreRightWrist);
+
+
     }
 }
 
@@ -68,5 +76,18 @@ function draw(){
             righthand.play();
             document.getElementById("song_pyed").innerHTML = "Crying for Rain";
         }
+    }
+
+    statusSong = righthand.isPlaying();//to store the info if the song is bieng played or not.
+
+    if(scoreRightWrist > 0.2){
+        circle(rightWrist_X,rightWrist_Y,25);
+        lefthand.stop();
+
+        if(lefthand.isPlaying() == false){
+            lefthand.play();
+            document.getElementById("song_pyed").innerHTML = "Mirror Heart";
+        }
+    
     }
 }
